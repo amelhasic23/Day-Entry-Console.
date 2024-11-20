db05283a6e551352006595be6ad65d83cb38e8f8
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-20 10:08:23 +0100

    Commit

---
+++ Day Entry/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite dan u nedelji (1-7):");
+
+        int dan;
+        if (int.TryParse(Console.ReadLine(), out dan))
+        {
+            string nazivDana = dan switch
+            {
+                1 => "Ponedeljak",
+                2 => "Utorak",
+                3 => "Sreda",
+                4 => "Četvrtak",
+                5 => "Petak",
+                6 => "Subota",
+                7 => "Nedelja",
+                _ => "Nepoznat dan"
+            };
+
+            Console.WriteLine($"Uneli ste dan: {nazivDana}");
+        }
+        else
+        {
+            Console.WriteLine("Uneli ste nevažeći broj. Pokušajte ponovo.");
+        }
+    }
+}
